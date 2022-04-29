af8d45f288c4bfc4f917038668bb6bd8b344b6df
Nick <Nick_II2000@mail.ru>
2022-04-29 21:00:47 +0600

    54-th commit

--- script.js
+++ script.js
@@ -260,3 +260,41 @@ function availableCurr(arr, missingCurr) {
 }
 
 console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'UAH'));
+
+// ---------------------------------------------------
+
+const shoppingMallData = {
+    shops: [{
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+function isBudgetEnough(data) {
+    let square = 0;
+    data.shops.forEach(item => square += square + item.width * item.length);
+    console.log(square);
+    if (data.budget >= square * data.height * moneyPer1m3) {
+        return 'Бюджета достаточно';
+    } else {
+        return 'Бюджета недостаточно';
+    }
+}
+
+console.log(isBudgetEnough(shoppingMallData));
