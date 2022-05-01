1931b4d2124c8ac4a923613443a8db308814450d
Nick <Nick_II2000@mail.ru>
2022-05-01 21:07:37 +0600

    67-th commit

--- script.js
+++ script.js
@@ -420,7 +420,7 @@ console.log(sortStudentsByGroups(students2));
 
 // ---------------------------------------------------
 
-const students3 = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam', 'Sam', 'Sam', 'Sam', 'Sam', 'Sam'];
+const students3 = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
 
 function sortStudentsByGroups(arr) {
     const resArr = [],
